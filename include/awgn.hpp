7afb56968ae9c1db71f6f5e04f118383a97e8714
Tapochek2894 <dvrogov06@yandex.ru>
2026-02-20 00:18:00 +0300

    added gitignore and awgn channel

---
+++ include/awgn.hpp
@@ -0,0 +1,23 @@
+#ifndef PUCH_F2_AWGN_HPP
+#define PUCH_F2_AWGN_HPP
+
+#include <complex>
+#include <cstdint>
+#include <random>
+#include <vector>
+
+namespace pucch_f2 {
+
+class AwgnChannel {
+public:
+    explicit AwgnChannel(double snr_db, uint32_t seed = 5489u);
+    std::vector<std::complex<double>> Transmit(const std::vector<std::complex<double>>& symbols);
+
+private:
+    std::mt19937 random_generator_;
+    double sigma_;
+};
+
+} // namespace pucch_f2
+
+#endif // PUCH_F2_AWGN_HPP
---
+++ src/awgn.cpp
@@ -0,0 +1,24 @@
+#include <awgn.hpp>
+
+namespace pucch_f2 {
+
+AwgnChannel::AwgnChannel(double snr_db, uint32_t seed) : random_generator_(seed) {
+    double snr_linear = std::pow(10.0, snr_db / 10.0);
+    sigma_ = std::sqrt(1.0 / (2.0 * snr_linear));
+}
+
+std::vector<std::complex<double>>
+AwgnChannel::Transmit(const std::vector<std::complex<double>>& symbols) {
+    std::vector<std::complex<double>> noisy_symbols;
+    std::normal_distribution<double> noise(0.0, sigma_);
+
+    for (const auto& symbol : symbols) {
+        double noise_re = noise(random_generator_);
+        double noise_im = noise(random_generator_);
+        noisy_symbols.emplace_back(symbol.real() + noise_re, symbol.imag() + noise_im);
+    }
+
+    return noisy_symbols;
+}
+
+} // namespace pucch_f2
